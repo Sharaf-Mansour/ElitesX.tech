57db287bcc62652391df45d41d44daa24c525694
Sharaf M. Mansour <sharaf_1015@yahoo.com>
2023-10-27 12:31:39 +0200

    coderub

--- src/routes/profile/[id]/+page.svelte
+++ src/routes/profile/[id]/+page.svelte
@@ -1,10 +1,10 @@
 <script>
-	import { page } from '$app/stores';
-    import * as Collection from  '$lib/index.js';
-  </script>
-  {#if Collection[$page.params.id]}
-<svelte:component this={Collection[$page.params.id]} />
+	import { page } from "$app/stores";
+	import * as Collection from "$lib/index.js";
+</script>
+
+{#if Collection[$page.params.id]}
+	<svelte:component this={Collection[$page.params.id]} />
 {:else}
-<h1>404</h1>
+	<h1>404</h1>
 {/if}
- 
